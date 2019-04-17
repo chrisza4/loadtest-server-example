fd41f5e8cffc29465f1d4b747b7492aa14998281
Chakrit Likitkhajorn <chakrit.lj@gmail.com>
2019-04-17 14:13:50 +0700

    Add test scripts

--- src/index.js
+++ src/index.js
@@ -3,5 +3,11 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/some-failed', (req, res) => {
+  if (Math.random() > 0.8) {
+    return res.status(500).send('Error')
+  }
+  return res.send('Success')
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))

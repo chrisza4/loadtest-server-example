ea15d104a9213d7057486be52953451901f0b6dc
Chakrit Likitkhajorn <chakrit.lj@gmail.com>
2019-04-18 11:41:14 +0700

    More

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 app.get('/some-failed', (req, res) => {
   if (Math.random() > 0.8) {
     return res.status(500).send('Error')
